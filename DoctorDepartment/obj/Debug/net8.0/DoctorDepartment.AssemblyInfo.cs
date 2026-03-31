f1226112d83657ddb8df00b8facaceee7857ea57
Gunel2727 <gunelemu@code.edu.az>
2026-03-31 23:45:01 +0400

    changing

--- DoctorDepartment/obj/Debug/net8.0/DoctorDepartment.AssemblyInfo.cs
+++ DoctorDepartment/obj/Debug/net8.0/DoctorDepartment.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DoctorDepartment")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+85917aa744f8082446aaf3e38e2114196ec1c841")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bfa7e3ffc859fdb42b779463bd2868cadf407607")]
 [assembly: System.Reflection.AssemblyProductAttribute("DoctorDepartment")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DoctorDepartment")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
